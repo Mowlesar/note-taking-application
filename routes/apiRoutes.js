548413487723a1f504c5ef2670bff74855544110
unknown <mowlesar.kathir@gmail.comcd bootcampOmit --git config mowlesar.kathir@gmail.com>
2023-08-24 11:24:34 -0400

    completed get and post routes for api's.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,45 @@
-const router = require('express').Router()
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const router = express.Router();
+
+const fs = require('fs');
+
+router.get('/api/notes', (req, res) => {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const notes = JSON.parse(data);
+    res.setHeader('Cache-Control', 'no-store');
+    console.log(notes)
+    res.json(notes);
+  });
+});
+
+router.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  let notes = [];
+
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    notes = JSON.parse(data);
+    newNote.id = uuidv4();
+    notes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.json(newNote);
+    });
+  });
+});
 
 
 module.exports = router;
